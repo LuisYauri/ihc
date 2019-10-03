898106e0e9e727984287bf0be77ffdb77859eda8
Luis Yauri <david.yauri147@gmail.com>
2019-10-03 00:46:17 -0500

    Estructura con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from './layouts/Login/login.component';
+import {LayoutsModule} from './layouts/layouts.module';
+import {SharedModule} from './shared/shared.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {StudentComponent} from './layouts/Student/student.component';
+import {TeacherComponent} from './layouts/Teacher/teacher.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  },
+  {
+    path: 'teacher',
+    component: TeacherComponent
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutsModule
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
